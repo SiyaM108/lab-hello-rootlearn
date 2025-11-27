f8c2facb677a96911d68abeec0cbad1c09c2042f
SiyaM108 <deprominent@gmail.com>
2025-11-27 23:09:43 +0000

    complete Hero.jsx, Navbar.jsx, Card.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Say hello to ReactJS</h1>
+      <p>You will learn how to use the most popular frontend library</p>
+      <button>Awesome!</button>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="/logo192.png" alt="React logo" className="logo" />
+    </nav>
+  );
+}
+
+export default Navbar;
