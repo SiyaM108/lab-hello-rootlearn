1684b5e77290ed4000952444670e66bad6cc60a2
SiyaM108 <deprominent@gmail.com>
2025-11-27 23:13:49 +0000

    complete App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+
+      <div className="cards">
+        <Card
+          img="https://imgur.com/H2L3ohY.png"
+          title="Declarative"
+          description="React makes it painless to create interactive UIs."
+        />
+        <Card
+          img="https://imgur.com/IHW1mFB.png"
+          title="Components"
+          description="Build encapsulated components that manage their own state."
+        />
+        <Card
+          img="https://imgur.com/LtUDB9y.png"
+          title="Single-Way"
+          description="A set of immutable values passed to the component’s."
+        />
+        <Card
+          img="https://imgur.com/agN6R4Y.png"
+          title="JSX"
+          description="Statically-typed, designed to run on modern browsers."
+        />
+      </div>
     </div>
   );
 }
