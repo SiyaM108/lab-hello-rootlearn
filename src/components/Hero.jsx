f7952ede421fa3156e7cdd31b40264d7ba4ba2ca
SiyaM108 <deprominent@gmail.com>
2025-11-28 16:52:05 +0000

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 function Hero() {
   return (
     <section className="hero">
-      <h1>Say hello to ReactJS</h1>
-      <p>You will learn how to use the most popular Frontend framework from scratch, to become a Ninka Developer</p>
-      <button>Awesome!</button>
+      <h1 class="header-1">Say hello to ReactJS</h1>
+      <p class="para-1">You will learn how to use the most popular Frontend framework from scratch, to become a Ninka Developer</p>
+      <button class="btn-1">Awesome!</button>
     </section>
   );
 }
