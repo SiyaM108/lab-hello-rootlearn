1ca264baa75ca1e50b2b27bd06b52442d0907e81
SiyaM108 <deprominent@gmail.com>
2025-11-29 11:19:40 +0000

    done

--- react-landing-page/src/components/Hero.jsx
+++ react-landing-page/src/components/Hero.jsx
@@ -1,9 +1,8 @@
 function Hero() {
   return (
     <header className="hero">
-      <h1>Say hello to ReactJS</h1>
-      <p>You will learn how to use the most popular frontend library.</p>
-      <p>and become a super Ninja developer.</p>
+      <h1>Say hello to <br></br>ReactJS</h1>
+      <p>You will learn how to use the most popular frontend framework <br></br>from scratch, to <br></br>become a super Ninja developer.</p>
 
       <button className="awesome-btn">Awesome!</button>
     </header>
